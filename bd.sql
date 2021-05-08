8979d8be29b63660404498f8f26c37df9df00ca7
programador51 <gskitcat@gmail.com>
2021-05-08 14:03:30 -0500

    BD

--- bd.sql
+++ bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-05-2021 a las 04:58:09
+-- Tiempo de generación: 08-05-2021 a las 21:02:52
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 8.0.1
 
@@ -20,6 +20,24 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `ste`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `idCategoria` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`idCategoria`, `nombre`) VALUES
+(1, 'Reparacion');
 
 -- --------------------------------------------------------
 
@@ -42,12 +60,234 @@ CREATE TABLE `clientes` (
 --
 
 INSERT INTO `clientes` (`idCliente`, `primerNombre`, `segundoNombre`, `apellidoPaterno`, `apellidoMaterno`, `numero`, `recomendadoPor`) VALUES
-(1, 'Jose ', 'Luis', 'Perez ', 'Olguin', 2147483647, 1),
+(1, 'Jose ', 'Luis', 'Perez ', 'Olguin Editado', 2147483647, 1),
 (2, 'Adrian', '', 'Alardin', 'Iracheta', 244545646, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contacto`
+--
+
+CREATE TABLE `contacto` (
+  `idContacto` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `contacto`
+--
+
+INSERT INTO `contacto` (`idContacto`, `nombre`) VALUES
+(1, 'Facebook'),
+(2, 'Whatsapp'),
+(3, 'Recomendacion');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dinero`
+--
+
+CREATE TABLE `dinero` (
+  `id` int(11) NOT NULL,
+  `montoInicial` int(10) NOT NULL,
+  `montoFinal` int(10) NOT NULL,
+  `dia` int(2) NOT NULL,
+  `mes` int(2) NOT NULL,
+  `yearTime` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estados`
+--
+
+CREATE TABLE `estados` (
+  `idEstado` int(11) NOT NULL,
+  `nombreEstado` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `estados`
+--
+
+INSERT INTO `estados` (`idEstado`, `nombreEstado`) VALUES
+(1, 'Apagado'),
+(2, 'Chip'),
+(3, 'Encendido'),
+(4, 'Sim');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `formaspago`
+--
+
+CREATE TABLE `formaspago` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `gastos`
+--
+
+CREATE TABLE `gastos` (
+  `idGasto` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `gestion`
+--
+
+CREATE TABLE `gestion` (
+  `id` int(11) NOT NULL,
+  `descripcion` longtext NOT NULL,
+  `gasto` int(20) NOT NULL,
+  `tipo` int(1) NOT NULL,
+  `dia` int(2) NOT NULL,
+  `mes` int(2) NOT NULL,
+  `yearData` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inventario`
+--
+
+CREATE TABLE `inventario` (
+  `codigo` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `descripcion` longtext NOT NULL,
+  `stock` int(20) NOT NULL,
+  `categoria` int(10) NOT NULL,
+  `precioCompra` float NOT NULL,
+  `precioVenta` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `inventario`
+--
+
+INSERT INTO `inventario` (`codigo`, `nombre`, `descripcion`, `stock`, `categoria`, `precioCompra`, `precioVenta`) VALUES
+(1, '', 'Kit brocas', 5, 1, 49.99, 99.99);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `movimientos`
+--
+
+CREATE TABLE `movimientos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `tipo` tinyint(4) NOT NULL,
+  `precio` int(10) NOT NULL,
+  `idCorte` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicios`
+--
+
+CREATE TABLE `servicios` (
+  `idServicio` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `descripcion` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `servicios`
+--
+
+INSERT INTO `servicios` (`idServicio`, `nombre`, `descripcion`) VALUES
+(1, 'Pantalla', 'Pantalla'),
+(2, 'Bateria', 'Bateria'),
+(3, 'Wifi', 'Wifi'),
+(4, 'Memoria', 'Memoria'),
+(5, 'Camara', 'Camara');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `idTicket` int(11) NOT NULL,
+  `diaRecoleccion` int(2) NOT NULL,
+  `mesRecoleccion` int(2) NOT NULL,
+  `recolectionYear` int(4) NOT NULL,
+  `diaEntrega` int(2) NOT NULL,
+  `mesEntrega` int(2) NOT NULL,
+  `entregaYear` int(4) NOT NULL,
+  `modelo` varchar(30) NOT NULL,
+  `descripcion` varchar(50) NOT NULL,
+  `servicio` varchar(50) NOT NULL,
+  `cotizacion` float NOT NULL,
+  `medioContactoId` int(11) DEFAULT NULL,
+  `medioPagoId` int(11) DEFAULT NULL COMMENT '[1-Efectivo]■[0-Tarjeta]',
+  `totalPago` float NOT NULL,
+  `clienteId` int(11) NOT NULL,
+  `estadoReparacion` tinyint(1) NOT NULL COMMENT '[0-Reparado] [1-No Reparado]',
+  `estadoEquipo` int(11) NOT NULL,
+  `nombreCliente` varchar(50) DEFAULT NULL,
+  `numeroCliente` int(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `ticket`
+--
+
+INSERT INTO `ticket` (`idTicket`, `diaRecoleccion`, `mesRecoleccion`, `recolectionYear`, `diaEntrega`, `mesEntrega`, `entregaYear`, `modelo`, `descripcion`, `servicio`, `cotizacion`, `medioContactoId`, `medioPagoId`, `totalPago`, `clienteId`, `estadoReparacion`, `estadoEquipo`, `nombreCliente`, `numeroCliente`) VALUES
+(16, 3, 5, 2021, 10, 5, 2021, 'FX2', 'No se ve ', '1', 400, 1, 0, 600, 1, 1, 1, 'Maria Leal Perez Coronado', 10202),
+(17, 17, 5, 2021, 24, 5, 2021, '8', 'Se borra la memoria', '4', 15000, 1, 0, 16000, 1, 0, 1, 'Juan Mario De leal Sanchez', 2147483647),
+(18, 26, 5, 2021, 27, 5, 2021, 'asdasdasdas', 'asdasdasd', '4', 123, 1, 1, 123, 1, 1, 1, 'Adrian  Alardin Iracheta', 2147483647);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ticketestados`
+--
+
+CREATE TABLE `ticketestados` (
+  `idTicketsEstado` int(11) NOT NULL,
+  `idEstadoTicketNombre` int(11) NOT NULL,
+  `ticketCorresponde` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`idCategoria`);
 
 --
 -- Indices de la tabla `clientes`
@@ -56,14 +296,181 @@ ALTER TABLE `clientes`
   ADD PRIMARY KEY (`idCliente`);
 
 --
+-- Indices de la tabla `contacto`
+--
+ALTER TABLE `contacto`
+  ADD PRIMARY KEY (`idContacto`);
+
+--
+-- Indices de la tabla `dinero`
+--
+ALTER TABLE `dinero`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `estados`
+--
+ALTER TABLE `estados`
+  ADD PRIMARY KEY (`idEstado`);
+
+--
+-- Indices de la tabla `formaspago`
+--
+ALTER TABLE `formaspago`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `gastos`
+--
+ALTER TABLE `gastos`
+  ADD PRIMARY KEY (`idGasto`);
+
+--
+-- Indices de la tabla `gestion`
+--
+ALTER TABLE `gestion`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  ADD PRIMARY KEY (`codigo`),
+  ADD KEY `categoria` (`categoria`);
+
+--
+-- Indices de la tabla `movimientos`
+--
+ALTER TABLE `movimientos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD PRIMARY KEY (`idServicio`);
+
+--
+-- Indices de la tabla `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`idTicket`);
+
+--
+-- Indices de la tabla `ticketestados`
+--
+ALTER TABLE `ticketestados`
+  ADD PRIMARY KEY (`idTicketsEstado`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
   MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `contacto`
+--
+ALTER TABLE `contacto`
+  MODIFY `idContacto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `dinero`
+--
+ALTER TABLE `dinero`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estados`
+--
+ALTER TABLE `estados`
+  MODIFY `idEstado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `formaspago`
+--
+ALTER TABLE `formaspago`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `gastos`
+--
+ALTER TABLE `gastos`
+  MODIFY `idGasto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `gestion`
+--
+ALTER TABLE `gestion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `movimientos`
+--
+ALTER TABLE `movimientos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  MODIFY `idServicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `idTicket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de la tabla `ticketestados`
+--
+ALTER TABLE `ticketestados`
+  MODIFY `idTicketsEstado` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `gastos`
+--
+ALTER TABLE `gastos`
+  ADD CONSTRAINT `gastos_ibfk_1` FOREIGN KEY (`idGasto`) REFERENCES `gestion` (`id`);
+
+--
+-- Filtros para la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  ADD CONSTRAINT `inventario_ibfk_1` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`idCategoria`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
