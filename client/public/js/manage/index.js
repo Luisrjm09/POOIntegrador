1d9523e5e852467eb04fb28c1ae9e06b37eaee47
programador51 <gskitcat@gmail.com>
2021-05-13 21:59:09 -0500

    Avanze para generar el reporte pdf

--- client/public/js/manage/index.js
+++ client/public/js/manage/index.js
@@ -2,6 +2,9 @@ const UI = require('../helpers/UI');
 const { getActualDate } = require('../helpers/date');
 const ManageUI = require('../helpers/Manage');
 const { formatMoney } = require('../helpers/numbers');
+const pdfMake = require('pdfmake/build/pdfmake.js');
+const pdfFonts = require('pdfmake/build/vfs_fonts.js');
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const today = getActualDate();
 
@@ -22,3 +25,16 @@ async function initialLoad(){
 initialLoad();
 
 document.getElementById('actualDate').innerHTML = today.stringDate;
+
+document.getElementById('btnGenerateReport').addEventListener('click',()=>{
+    /* pdfMake.vfs = pdfFonts.pdfMake.vfs; */
+
+    const htmlStructure = {
+        content: [
+		'First paragraph',
+		'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+	]
+    }
+
+    pdfMake.createPdf(htmlStructure).download();
+})
